21c868ffce12fa21432d00f9c23f0c4fe05b9dde
fanlizhou <fanlizhou@yunqilaohe.com>
2024-08-27 11:06:35 +0800

    refactor: refactor session management with optional name parameter
    - Remove the statement about automatically saving the active session from the README
    - Add an optional `name` parameter to session-related functions
    - Refactor the `current` function to handle the optional `name` parameter
    - Remove the `active`, `start`, and `stop` functions
    - Refactor the `save` function to handle the optional `name` parameter and replace the logic in the `start` function
    - Update the `load` function to handle the optional `name` parameter
    - Remove the `select` function
    
    Signed-off-by: fanlizhou <fanlizhou@yunqilaohe.com>

--- lua/persistence/init.lua
+++ lua/persistence/init.lua
@@ -3,15 +3,21 @@ local Config = require("persistence.config")
 local uv = vim.uv or vim.loop
 
 local M = {}
-M._active = false
 
 local e = vim.fn.fnameescape
 
----@param opts? {branch?: boolean}
+---@param opts? {branch?: boolean, name?: string}
 function M.current(opts)
   opts = opts or {}
-  local name = vim.fn.getcwd():gsub("[\\/:]+", "%%")
-  if Config.options.branch and opts.branch ~= false then
+
+  local name
+  if opts.name then
+    name = opts.name:gsub("[\\/:]+", "%%")
+  else
+    name = vim.fn.getcwd():gsub("[\\/:]+", "%%")
+  end
+
+  if Config.options.branch or opts.branch ~= false then
     local branch = M.branch()
     if branch and branch ~= "main" and branch ~= "master" then
       name = name .. "%%" .. branch:gsub("[\\/:]+", "%%")
@@ -22,7 +28,6 @@ end
 
 function M.setup(opts)
   Config.setup(opts)
-  M.start()
 end
 
 function M.fire(event)
@@ -31,46 +36,15 @@ function M.fire(event)
   })
 end
 
--- Check if a session is active
-function M.active()
-  return M._active
+---@param opts? {branch?: boolean, name?: string}
+function M.save(opts)
+  opts = opts or {}
+  M.fire("SavePre")
+  vim.cmd("mks! " .. e(M.current(opts)))
+  M.fire("SavePost")
 end
 
-function M.start()
-  M._active = true
-  vim.api.nvim_create_autocmd("VimLeavePre", {
-    group = vim.api.nvim_create_augroup("persistence", { clear = true }),
-    callback = function()
-      M.fire("SavePre")
-
-      if Config.options.need > 0 then
-        local bufs = vim.tbl_filter(function(b)
-          if vim.bo[b].buftype ~= "" or vim.bo[b].filetype == "gitcommit" or vim.bo[b].filetype == "gitrebase" then
-            return false
-          end
-          return vim.api.nvim_buf_get_name(b) ~= ""
-        end, vim.api.nvim_list_bufs())
-        if #bufs < Config.options.need then
-          return
-        end
-      end
-
-      M.save()
-      M.fire("SavePost")
-    end,
-  })
-end
-
-function M.stop()
-  M._active = false
-  pcall(vim.api.nvim_del_augroup_by_name, "persistence")
-end
-
-function M.save()
-  vim.cmd("mks! " .. e(M.current()))
-end
-
----@param opts? { last?: boolean }
+---@param opts? { branch?: boolean, last?: boolean, name?: string }
 function M.load(opts)
   opts = opts or {}
   ---@type string
@@ -78,9 +52,9 @@ function M.load(opts)
   if opts.last then
     file = M.last()
   else
-    file = M.current()
+    file = M.current(opts)
     if vim.fn.filereadable(file) == 0 then
-      file = M.current({ branch = false })
+      file = M.current({ branch = false, name = opts.name })
     end
   end
   if file and vim.fn.filereadable(file) ~= 0 then
@@ -101,37 +75,6 @@ end
 
 function M.last()
   return M.list()[1]
-end
-
-function M.select()
-  ---@type { session: string, dir: string, branch?: string }[]
-  local items = {}
-  local have = {} ---@type table<string, boolean>
-  for _, session in ipairs(M.list()) do
-    if uv.fs_stat(session) then
-      local file = session:sub(#Config.options.dir + 1, -5)
-      local dir, branch = unpack(vim.split(file, "%%", { plain = true }))
-      dir = dir:gsub("%%", "/")
-      if jit.os:find("Windows") then
-        dir = dir:gsub("^(%w)/", "%1:/")
-      end
-      if not have[dir] then
-        have[dir] = true
-        items[#items + 1] = { session = session, dir = dir, branch = branch }
-      end
-    end
-  end
-  vim.ui.select(items, {
-    prompt = "Select a session: ",
-    format_item = function(item)
-      return vim.fn.fnamemodify(item.dir, ":p:~")
-    end,
-  }, function(item)
-    if item then
-      vim.fn.chdir(item.dir)
-      M.load()
-    end
-  end)
 end
 
 --- get current branch name
